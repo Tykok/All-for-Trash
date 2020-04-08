8f3e9681e380b2fb9009b213390955bd8e25db06
Elie Treport <treportelie.pro@outlook.com>
2020-04-08 18:48:19 +0400

    Bouton
    Test des boutons

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 
     <title> All for trash </title>
 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <link rel="stylesheet" href="lib/css/materialize.min.css">
     <script src="lib/js/materialize.min.js"></script>
 
@@ -18,13 +20,21 @@
         <div class="nav-wrapper">
             <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
             <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-                <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-                <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-                <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+                <li><a href="#"><i class="material-icons">search</i></a></li>
+                <li><a href="#"><i class="material-icons">view_module</i></a></li>
+                <li><a href="#"><i class="material-icons">refresh</i></a></li>
+                <li><a href="#"><i class="material-icons">more_vert</i></a></li>
             </ul>
         </div>
     </nav>
+
+
+
+    <a class="waves-effect waves-light btn">button</a>
+    <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+    <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+
+    <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
 
 
 
