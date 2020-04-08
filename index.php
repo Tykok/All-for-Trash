22d878310eaa1ad96d8bf2c28cb71dddc67750e5
Elie Treport <treportelie.pro@outlook.com>
2020-04-08 18:33:22 +0400

    Materialize navbar
    A simple navbar

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <html>
+
 <head>
-<title> All for trash </title>
+
+    <title> All for trash </title>
+
+    <link rel="stylesheet" href="lib/css/materialize.min.css">
+    <script src="lib/js/materialize.min.js"></script>
+
 </head>
 
 <body>
 
+
+
+
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="sass.html"><i class="material-icons">search</i></a></li>
+                <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+                <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+                <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+
 </body>
+
+
+<script>
+    $(document).ready(function() {
+        $('.sidenav').sidenav();
+    });
+</script>
+
 </html>
