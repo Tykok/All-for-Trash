df9d767b0641dcccc8c58593a56df8b0f8b9915b
Tykok <56304246+Tykok@users.noreply.github.com>
2020-04-29 16:10:28 +0400

    Script de géocalisation sur la vue view.principal.php

--- view/view.principal.php
+++ view/view.principal.php
@@ -8,5 +8,34 @@ Idées pour la page principal :
     <li>Présentation des déchets les plus repérer</li>
     <li>Nb de connexion sur notre site</li>
     <li>Nb d'utilisateurs</li>
-    
+
 </ul>
+
+
+<h3>Gélocalisation de l'utilisateur</h3>
+<p id="demo"></p>
+
+
+
+<script>
+    function geo_success(position) {
+        showPosition(position);
+    }
+
+    function geo_error() {
+        alert("Sorry, no position available.");
+    }
+
+    var geo_options = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    };
+
+    var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+
+    function showPosition(position) {
+        $('#demo').html("Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude);
+    }
+</script>
