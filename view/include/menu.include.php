75548785f732c96d331f37fbb969da21f2a3bc34
Tykok <56304246+Tykok@users.noreply.github.com>
2020-05-10 01:17:44 +0400

    Modal de connexion user à améliorer niveau style
    AUCUNE VERIFICATION, juste le gabarit pour la connexion user

--- view/include/menu.include.php
+++ view/include/menu.include.php
@@ -1,7 +1,7 @@
 <!-- BARRE DE NAVIGUATION ECRAN ORDINATEUR -->
 <nav>
     <div class="navbar-fixed grey darken-3">
-        <a href="?page=connexion" class="brand-logo right">
+        <a href="#modal_connexion" class="modal-trigger brand-logo right">
             <i class="large right material-icons">account_box</i>Connexion
         </a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
@@ -19,20 +19,20 @@
 
     <li>
         <a href="index.php" style='background-color:black' class="brand-logo">
-            <img class="responsive-img" src='<?php echo img . "trash-logo.jpg"; ?>' width="285">
+            <img class="responsive-img" src='<?php echo img . "principal/trash-logo.jpg"; ?>' width="285">
         </a>
     </li>
 
     <li><a href="?" class="toast-basic">Acceuil</a></li>
-    <li><a href="?page=connexion" class="toast-basic"><i class="material-icons">account_box</i>Connexion</a></li>
+    <li><a href="#modal_connexion" class="modal-trigger toast-basic"><i class="material-icons">account_box</i>Connexion</a></li>
 </ul>
 
 
-
+<!-- Notre modal de connexion -->
+<?php require_once(modal . 'connexion.html'); ?>
 
 <!--  Menu normal puis au format "hamburger" quand le format est plus petit -->
 <script>
-    // A appeler après l'appel du JQuery!
     $(document).ready(function() {
         $('.sidenav').sidenav();
         $(".dropdown-trigger").dropdown();
